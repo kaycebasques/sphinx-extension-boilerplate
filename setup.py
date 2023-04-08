a96f7bfa3324d6c026d2a213ae7538557ddc7b1e
Kayce Basques <kayce@google.com>
2023-04-08 14:27:16 -0700

    Specify llama-index as a dependency

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='extotype',
-    version='0.4',
+    version='0.5',
     packages=find_packages(),
-    install_requires=['sphinx'],
+    install_requires=[
+        'sphinx',
+        'llama-index'
+    ],
     classifiers=[],
     package_data={
         'extotype': [
