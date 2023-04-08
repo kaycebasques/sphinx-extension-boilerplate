e5904967eed81577dac430f028dce33232903a6b
Kayce Basques <kayce@google.com>
2023-04-08 16:01:50 -0700

    Generate checksums for each text output

--- extotype/__init__.py
+++ extotype/__init__.py
@@ -1,13 +1,30 @@
 from os import path
 from sphinx.jinja2glue import SphinxFileSystemLoader
 from sphinx.application import Sphinx
-# from docutils import nodes
-# from hashlib import md5
+from docutils import nodes
+from hashlib import md5
 from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
+from json import dumps
+from pathlib import Path
 
 data = {}
 
+def xyz(app, exception):
+    if app.builder.name != 'text':
+        return
+    print(app.outdir)
+    for p in Path(app.outdir).glob('**/*.txt'):
+        absolute_path = p.as_posix()
+        relative_path = absolute_path.replace(app.outdir, '')
+        with open(absolute_path, 'r') as f:
+            content = f.read()
+        data[relative_path] = {
+            'checksum': md5(content.encode('utf-8')).hexdigest()
+        }
+    print(dumps(data, indent=2))
+
 # def generate_embeddings(app, doctree, docname):
+#     print(doctree.astext())
 #     data[docname] = {}
 #     for section in doctree.traverse(nodes.section):
 #         text = section.astext()
@@ -26,19 +43,20 @@ def add_static_dir(app):
     app.builder.templates.loaders.insert(1, SphinxFileSystemLoader(template_dir))
     app.builder.templates.templatepathlen += 1
 
-def generate_index(app, exception):
-    if app.builder.name != 'text':
-        return
-    documents = SimpleDirectoryReader(app.outdir).load_data()
-    index = GPTSimpleVectorIndex.from_documents(documents)
-    index.save_to_disk(path.join(app.outdir, 'index.json'))
+# def generate_index(app, exception):
+#     if app.builder.name != 'text':
+#         return
+#     documents = SimpleDirectoryReader(app.outdir).load_data()
+#     index = GPTSimpleVectorIndex.from_documents(documents)
+#     index.save_to_disk(path.join(app.outdir, 'index.json'))
 
 def setup(app):
     app.connect('builder-inited', add_static_dir)
     # app.connect('doctree-resolved', generate_embeddings)
-    app.connect('build-finished', generate_index)
+    # app.connect('build-finished', generate_index)
+    app.connect('build-finished', xyz)
     return {
-        'version': '0.3',
+        'version': '0.1',
         'parallel_read_safe': True,
         'parallel_write_safe': True,
     }
